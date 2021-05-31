8c90f5106f8cf47858b662cc2903721bdc3e608a
Gustavo-program <gustavo.roman2@outlook.es>
2021-05-30 21:38:37 -0500

    hola

---
+++ src/soap/cibertec/servicio/servicioDocente.java
@@ -0,0 +1,10 @@
+package soap.cibertec.servicio;
+import java.util.List;
+import soap.cibertec.dao.DistritoDAOImpl;
+import soap.cibertec.dao.DocenteDAOImpl;
+import soap.cibertec.entidad.DistritoBean;
+import soap.cibertec.entidad.DocenteBean;
+public class servicioDocente {
+	
+}
+
---
+++ src/utils/MysqlDBConexion.java
@@ -0,0 +1,16 @@
+package utils;
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class MysqlDBConexion {
+	public static Connection getConexion(){
+		Connection cn=null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			cn=DriverManager.getConnection("jdbc:mysql://localhost/instituto_2019_22?serverTimezone=UTC","root","mysql");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return cn;	
+	}
+}
+
